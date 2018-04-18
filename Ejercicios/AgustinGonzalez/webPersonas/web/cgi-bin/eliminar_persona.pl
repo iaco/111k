dc89041cd14deafb90c5ee88f57284d025761370
agonzalez1988 <agustin17.05@gmail.com>
2018-04-18 15:23:19 -0300

    Ejercicio web funcionando con los .pl

---
+++ Ejercicios/AgustinGonzalez/webPersonas/web/cgi-bin/eliminar_persona.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+use lib "./../controller";
+use Interprete;
+use CGI;
+
+my $cgi = new CGI;
+my $servidor= new Interprete;
+
+#&parse_form_data(*simple_form);
+
+print  "Content-type: text/html", "\n\n";
+
+#$id =$simple_form{"id"};
+my $id = $cgi->param("id");
+
+my $funciono=$servidor->eliminar_persona($id);
+
+
+print '<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>..::Listado de personas:..</title>
+        <link rel="stylesheet" type="text/css" href="../CSS/Styles.css">
+
+    </head>
+    <body>';
+    if ($funciono)
+    {
+        print "<p> persona eliminada correctamente </p>";
+    }
+    else
+    {
+        print "<p> Error desconocido </p>";
+    }
+    print '<a href="index.pl">VOLVER </a>
+    </body>
+    </html>';
+
+
+
+
+
+
+sub parse_form_data
+{
+    local (*FORM_DATA) = @_;
+    local ( $request_method, $query_string, @key_value_pairs,
+                  $key_value, $key, $value);
+    $request_method = $ENV{'REQUEST_METHOD'};
+    if ($request_method eq "GET") {
+        $query_string = $ENV{'QUERY_STRING'};
+    } elsif ($request_method eq "POST") {
+        read (STDIN, $query_string, $ENV{'CONTENT_LENGTH'});
+    } else {
+        &return_error (500, "Server Error",
+                            "Server uses unsupported method");
+    }
+    @key_value_pairs = split (/&/, $query_string);
+    foreach $key_value (@key_value_pairs) {
+        ($key, $value) = split (/=/, $key_value);
+        $value =~ tr/+/ /;
+        $value =~ s/%([\dA-Fa-f][\dA-Fa-f])/pack ("C", hex ($1))/eg;
+        if (defined($FORM_DATA{$key})) {
+            $FORM_DATA{$key} = join ("\0", $FORM_DATA{$key}, $value);
+        } else {
+                    $FORM_DATA{$key} = $value;
+        }
+    }
+}
+
+
+sub return_error
+{
+    local ($status, $keyword, $message) = @_;
+    print "Content-type: text/html", "\n";
+    print "Status: ", $status, " ", $keyword, "\n\n";
+    print "<<End_of_Error
+    <HTML>
+    <HEAD>
+        <TITLE>CGI Program - Unexpected Error</TITLE>
+    </HEAD>
+    <BODY>
+    <H1>$keyword</H1>
+    <HR>$message<HR>
+    Please contact $webmaster for more information.
+    </BODY>
+    </HTML>
+    End_of_Error";
+    exit(1);
+}
---
+++ Ejercicios/AgustinGonzalez/webPersonas/web/cgi-bin/index.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use lib "./../controller";
+use Interprete;
+use CGI;
+use HTML::Template;
+
+my $cgi= new CGI;
+my $template = new HTML::Template(filename=>"form.tmpl");
+
+my $servidor= new Interprete;
+
+my @personas = $servidor->get_lista;
+
+$template->param(ROWS => \@personas);
+
+#print "Content-type: text/html\n\n";
+#print &encabezado;
+print $cgi->header(-charset    => 'utf-8');
+print $template->output;
+#print &cierre;
+
+
+
+
+
+sub encabezado
+{
+    my $encabezado = '<!DOCTYPE  html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width", initial-scale=1.0">
+        <title>..::Listado de personas:..</title>
+        <link rel="stylesheet" type="text/css" href="/CSS/Styles.css">
+    </head>
+
+    <body>';
+    return $encabezado;
+}
+
+sub cierre
+{
+    my $cierre = '    </body>
+            </html>';
+            return $cierrre;
+}
