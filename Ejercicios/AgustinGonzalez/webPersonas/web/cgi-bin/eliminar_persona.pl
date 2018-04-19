c0e29b75a1d6c4e6553ee1d1a67cfae3fda8386a
agonzalez1988 <agustin17.05@gmail.com>
2018-04-19 18:12:44 -0300

    Cambio a la pagina principal para enlazar los cgi

--- Ejercicios/AgustinGonzalez/webPersonas/web/cgi-bin/eliminar_persona.pl
+++ Ejercicios/AgustinGonzalez/webPersonas/web/cgi-bin/eliminar_persona.pl
@@ -20,7 +20,6 @@ print '<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>..::Listado de personas:..</title>
         <link rel="stylesheet" type="text/css" href="../CSS/Styles.css">
 
     </head>
